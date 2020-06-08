cb5a4fcc2fb96a675befb396b8f003ce1c5b3f37
paragthakkar <parag_thakkar99@hotmail.com>
2020-06-08 16:43:46 +0530

    Add mavenJenkinsDemo

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+package main.java;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class HelloWorld {
+    public static void main(String[] args) throws ArithmeticException{
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyTest {
+
+    @Test
+    public void test1() {
+        Assert.assertTrue(true);
+    }
+}
